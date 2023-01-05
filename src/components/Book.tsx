6f33441486098c020c397d70c1ff2beb509ffee0
gandy818 <gandy818@gmail.com>
2023-01-05 15:20:58 +0900

    design : 모달창 디자인 작업

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -16,8 +16,8 @@ const Book = ({ item }: IProps) => {
 
   return (
     <>
-      <div className=" bg-one-red my-3 mx-2 box-border h-fit max-h-full w-48 border border-solid shadow-lg">
-        <div className="m-2 mb-3 items-center text-center justify-between text-xl font-bold text-slate-600">
+      <div>
+        <div>
           <CardTitle>{item.title}</CardTitle>
         </div>
         <div>
