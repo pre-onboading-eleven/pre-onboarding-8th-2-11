7b1d1179f02dbecaa62dcf4298bd7c96a80ac689
gandy818 <gandy818@gmail.com>
2023-01-06 18:39:30 +0900

    refactor : app.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,6 @@ function App() {
         <Nav />
         <Routes>
           <Route index path="/" element={<Home />} />
-          {/* <Route path="/add" element={<AddIssueInput />} /> */}
-          <Route path="/issue/:id" element={<Detail />} />
         </Routes>
       </Router>
     </div>
